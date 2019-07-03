34c087f66c4e21be186a923e18819965c320fc81
Duc Vu <de.yd315@gmail.com>
2019-07-03 16:25:42 +0700

    fixed issue with deployment port

--- src/index.js
+++ src/index.js
@@ -11,18 +11,25 @@ var http = require('http');
 /**
  * Get port from environment and store in Express.
  */
-var port = 5000;
+var port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "development") {
-    port = 3000
+
+if (!port) {
+    if (process.env.NODE_ENV === "production") {
+        port = 80
+    }
+
+    if (process.env.NODE_ENV === "development") {
+        port = 3000;
+    }
 }
-
-if (process.env.NODE_ENV === "production") {
-    port = 80
-}
-
 
 app.set('port', port);
+
+
+
+
+
 
 /**
  * Create HTTP server.
