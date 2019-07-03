74e37ee4ca8e09eb9fc071acd08ee12efa6cca73
Duc Vu <de.yd315@gmail.com>
2019-07-03 13:09:03 +0700

    integrate mongodb

--- app.js
+++ app.js
@@ -4,6 +4,25 @@ import path from 'path';
 import withMiddleware from './_helpers/withMiddleware'
 import withRoutes from './routes';
 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ducvu:b3stAdminH3r3@cluster0-yftd6.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    useNewUrlParser: true
+});
+client.connect(err => {
+    if (err) {
+        // console.log('failed to connect to db')
+        return;
+    }
+
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+
+
+    client.close();
+});
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
