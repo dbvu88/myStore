c4daa340cd1e4455f7c526205ace4ed2b310f4fd
Duc Vu <de.yd315@gmail.com>
2019-07-04 11:08:21 +0700

    updated

---
+++ src/_helpers/hashPassword.js
@@ -0,0 +1,17 @@
+import bcrypt from "bcrypt";
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+export const hashPassword = (password, callback) => {
+    console.log('password hashing')
+
+    return bcrypt.hash(
+        password,
+        saltRounds,
+        (err, hash) => {
+            if (err) callback(err)
+            else callback(hash)
+        }
+    );
+}
