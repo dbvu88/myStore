c1980d6928cf1f69c4e2ee1561638c445409f913
Duc Vu <de.yd315@gmail.com>
2019-07-04 11:07:03 +0700

    added testing file

---
+++ src/app.test.js
@@ -0,0 +1,42 @@
+import request from 'supertest'
+import app from './app'
+
+
+describe('the rootpath', () => {
+    test('should response with status 200', () => {
+        return request(app).get('/').expect(200);
+    });
+})
+
+describe('the POST auth/register route', () => {
+    test('should response with the hashed pass', () => {
+        return request(app)
+            .post('/auth/register')
+            .send({
+                username: 'duc',
+                pasword: '123'
+            }).expect(200)
+    })
+})
+
+// it('will check the matchers and pass', () => {
+//     const user = {
+//         createdAt: new Date(),
+//         id: Math.floor(Math.random() * 20),
+//         name: 'LeBron James',
+//     };
+
+//     expect(user).toMatchSnapshot({
+//         createdAt: expect.any(Date),
+//         id: expect.any(Number),
+//     });
+// });
+
+// // Snapshot
+// exports[`will check the matchers and pass 1`] = `
+//   Object {
+//     "createdAt": Any<Date>,
+//     "id": Any<Number>,
+//     "name": "LeBron James",
+//   }
+//   `;
