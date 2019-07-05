623cbb130555e873ed256df21118c27f91fa07bb
Duc Vu <de.yd315@gmail.com>
2019-07-05 15:06:59 +0700

    fixed the rooth endpoint issue

--- src/_helpers/applyMiddleware.js
+++ src/_helpers/applyMiddleware.js
@@ -2,11 +2,9 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
-import path from 'path';
 
 
 export default (app, express) => {
-  app.use(express.static(path.join(__dirname, '../public')));
   app.use(express.json());
   app.use(express.urlencoded({
     extended: false,
--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 import _helpers from './_helpers';
 import withRoutes from './routes';
 // import './_db';
 
-
 const app = express();
 
-_helpers.applyMiddleware(app, express);
+app.use(express.static(path.join(__dirname, '../public')));
+_helpers.applyMiddleware(app, express, path);
 
 withRoutes(app);
 
