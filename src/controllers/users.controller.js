222e3c4817dcbcdaecbdb27a8d918910e7f66aed
ducbavu531@gmail.com <de.yd315@gmail.com>
2019-07-06 18:42:55 +0700

    refactored code to be more readable

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,7 +2,13 @@ import 'regenerator-runtime';
 import User from '../models/user.model';
 
 const get = async () => {
-  const data = await User.find().select('-password');
+  const data = await User
+    .find()
+    .select('-password')
+    .then(users => users)
+    .catch((err) => {
+      throw err;
+    });
   return data;
 };
 
