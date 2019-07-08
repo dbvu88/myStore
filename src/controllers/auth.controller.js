8050cde48ab3b1fd0946ca5d9b433dccd56fa6bd
Duc Vu <de.yd315@gmail.com>
2019-07-08 18:06:28 +0700

    fixed auth/regiter to handle errors properly

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,34 +1,55 @@
 import _helpers from '../_helpers';
-import Users from '../models/user.model';
-import 'regenerator-runtime';
+import User from '../models/user.model';
 
-
-const register = async ({ password, ...userInfo }) => {
-  try {
-    const result = await _helpers.getHash(password);
-
-    const user = await Users.create({
-      password: result,
-      ...userInfo,
+const login = async (username, password) => {
+  await _helpers.getHash(password)
+    .then(async (hash) => {
+      console.log(hash);
+      const user = await User
+        .find({ username, password: hash })
+        .select('-password');
+      console.log(user);
+    })
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((err) => {
+      throw err;
     });
+};
+
+const register = user => new Promise(async (resolve, reject) => {
+  const { password, ...userInfo } = user;
+  if (!password) {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    reject({ msg: 'Must Enter a Password' });
+  }
+  try {
+    const hash = await _helpers.getHash(password);
     const {
       _id: id,
       firstname,
       lastname,
       username,
-    } = user;
+    } = await new User({
+      password: hash,
+      ...userInfo,
+    }).save();
 
-    const token = await _helpers.getToken(id);
-    return {
+    const token = await _helpers.createSession(id);
+
+    resolve({
       token,
       firstname,
       lastname,
       username,
-    };
+    });
   } catch (err) {
-    throw err;
+    reject(err || { msg: 'Registration Failed' });
   }
-};
+});
+
 export default {
   register,
+  login,
 };
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -2,19 +2,30 @@ import express from 'express';
 import Auth from '../controllers/auth.controller';
 
 const router = express.Router();
-const register = async (req, res, next) => {
-  try {
-    const data = await Auth.register(req.body);
-    res.json(data);
-  } catch (err) {
-    res.json(err);
+const login = async (req, res, next) => {
+  const { username, password } = req.body;
+  if (username && password) {
+    await Auth
+      .login(username, password)
+      .then(data => res.json(data))
+      .catch(err => res.status(401).json(err));
   }
+  res
+    .status(404)
+    .json({ errmsg: 'username and password are required' });
 };
+
+const register = (req, res, next) => Auth.register(req.body)
+  .then((data) => { res.json(data); })
+  .catch((err) => {
+    // eslint-disable-next-line no-param-reassign
+    err.status = 404;
+    next(err);
+  });
+
 
 router
   .post('/register', register)
-  .post('/login', (req, res) => {
-    res.send('login');
-  });
+  .post('/login', login);
 
 export default router;
